ad2a7d370108af33d98b5b454617213cdddcadd1
Tom Grother <tom@designandsupply.co.uk>
2018-05-25 17:54:26 +0100

    Class Added to allow a user to be selected before updating stock. Class statically holds the full name and id of the user for later use

---
+++ WindowsFormsApp2/AuthenticatedUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    class AuthenticatedUser
+    {
+        private string _fullName;
+        public static string uFullName { get; set; }
+
+
+        public static int uID { get; set; }
+
+        public AuthenticatedUser(int userId)
+        {
+            uID = userId;
+            uFullName = userFullName();
+        }
+
+        private string userFullName()
+        {
+            SqlConnection conn = new SqlConnection(SqlStatements.ConnectionStringUser);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT id,forename + ' ' + surname as fullname from dbo.user WHERE id = @id;";
+            cmd.Parameters.AddWithValue("@id", uID);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.Read())
+            {
+                return rdr["fullname"].ToString();
+            }
+            else
+            {
+                return "";
+            }
+
+            
+        }
+
+
+
+    }
+}
